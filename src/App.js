cf905ac91ecaf52cf2141cba53b962379714771e
Oualid-Amzil <raskolanikouf2@gmail.com>
2023-10-30 18:45:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState } from "react";
+import OpenAI from "openai";
+import "./App.css";
 
 function App() {
+  const [imgs, setImgs] = useState([]);
+
+  const openai = new OpenAI({
+    apiKey: "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",
+    dangerouslyAllowBrowser: true,
+  });
+
+  const getImages = async () => {
+    try {
+      const image = await openai.images.generate({
+        prompt: "A cute baby sea otter",
+        size: "256x256",
+        n: 2,
+      });
+
+      setImgs(image.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  getImages();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>first interact with ChatGPT</h1>
+      <div className="imgs__container">
+        {imgs.map((ele) => (
+          <img key={imgs.indexOf(ele)} src={ele.url} alt="element" />
+        ))}
+      </div>
     </div>
   );
 }
